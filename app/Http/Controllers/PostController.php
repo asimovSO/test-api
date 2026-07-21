a4cd14807047d097b44c4edb397b8aa7098b12bb
asimovSO <svoleg2002@gmail.com>
2026-07-21 18:31:39 +0300

    Post create functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,12 +8,21 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //
-    public function createPost(Request $request){
-        $fields = $request->only([
-            'title', 'body', 'poster'
-        ]);
+    public function createPost(Request $request)
+    {
+        $fields = array_merge(
+            ['body' => null, 'poster' => null],
+            $request->validate([
+                'title' => 'required|string|max:255',
+                'body' => 'sometimes|string|max:3000',
+                'poster' => 'sometimes|string|max:255',
+            ])
+        );
 
-        $post = Post::create($fields);
+        $post = Post::create([
+            'user_id' => $request->user()->id,
+            ...$fields,
+        ]);
 
         return response()->json([
             'message' => 'Post created.',
