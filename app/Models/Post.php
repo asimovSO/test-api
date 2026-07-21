9e544ebac89df480169911712d1a3d04ce0f2415
asimovSO <svoleg2002@gmail.com>
2026-07-21 23:07:59 +0300

    Append image to post functionality

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,12 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'body', 'poster', 'user_id'];
+    protected $appends = ['poster_url'];
 
-    public function user(){
-        $this->belongsTo(User::class);
+    public function getPosterUrlAttribute(): ?string
+    {
+        return $this->poster ? Storage::disk('public')->url($this->poster) : null;
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
